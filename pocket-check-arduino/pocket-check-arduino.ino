e3cb408b0f8e592fe2140c092c5ac5db35665e5b
Jingwen <zjwwjz@live.cn>
2015-10-25 12:37:08 -0400

    init

---
+++ pocket-check-arduino/pocket-check-arduino.ino
@@ -0,0 +1,258 @@
+/*********************************************************************
+ This is an example for our nRF51822 based Bluefruit LE modules
+
+ Pick one up today in the adafruit shop!
+
+ Adafruit invests time and resources providing this open source code,
+ please support Adafruit and open-source hardware by purchasing
+ products from Adafruit!
+
+ MIT license, check LICENSE for more information
+ All text above, and the splash screen below must be included in
+ any redistribution
+*********************************************************************/
+
+#include <Arduino.h>
+#include <SPI.h>
+#if not defined (_VARIANT_ARDUINO_DUE_X_) && not defined (_VARIANT_ARDUINO_ZERO_)
+#include <SoftwareSerial.h>
+#endif
+
+#include "Adafruit_BLE.h"
+#include "Adafruit_BluefruitLE_SPI.h"
+#include "Adafruit_BluefruitLE_UART.h"
+
+#include "BluefruitConfig.h"
+
+#define FACTORYRESET_ENABLE         0
+#define MINIMUM_FIRMWARE_VERSION    "0.6.6"
+#define MODE_LED_BEHAVIOUR          "MODE"
+
+#define BLUEFRUIT_HWSERIAL_NAME           Serial1
+#define BLUEFRUIT_UART_MODE_PIN         -1   // Not used with FLORA
+#define BLUEFRUIT_UART_CTS_PIN          -1   // Not used with FLORA
+#define BLUEFRUIT_UART_RTS_PIN          -1   // Not used with FLORA
+
+// Create the bluefruit object, with hardware serial, which does not need the RTS/CTS pins. */
+Adafruit_BluefruitLE_UART ble(Serial1, BLUEFRUIT_UART_MODE_PIN);
+
+/**************************************************************************/
+
+//NeoPixel Setup
+#include <Adafruit_NeoPixel.h>
+#include <avr/power.h>
+#define PIN            10
+#define NUMPIXELS      5
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+//Address specific pixel for corresponding item
+#define phonePixel 0
+#define umbrellaPixel 3
+boolean rain = false;
+
+//Button for checking
+#define buttonPin 6
+int buttonState = 1;
+//boolean newBuffer = false;
+boolean startRanging = 1;
+
+/**************************************************************************/
+
+// A small helper
+void error(const __FlashStringHelper*err) {
+  Serial.println(err);
+  while (1);
+}
+
+/**************************************************************************/
+/*!
+    @brief  Sets up the HW an the BLE module (this function is called
+            automatically on startup)
+*/
+/**************************************************************************/
+void setup(void)
+{
+  pinMode(buttonPin, INPUT);
+  pixels.begin(); // This initializes the NeoPixel library.
+
+  while (!Serial);  // required for Flora & Micro
+  delay(500);
+
+  Serial.begin(115200);
+  Serial.println(F("Adafruit Bluefruit Command Mode Example"));
+  Serial.println(F("---------------------------------------"));
+
+  /* Initialise the module */
+  Serial.print(F("Initialising the Bluefruit LE module: "));
+
+  if ( !ble.begin(VERBOSE_MODE) )
+  {
+    error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
+  }
+  Serial.println( F("OK!") );
+
+  if ( FACTORYRESET_ENABLE )
+  {
+    /* Perform a factory reset to make sure everything is in a known state */
+    Serial.println(F("Performing a factory reset: "));
+    if ( ! ble.factoryReset() ) {
+      error(F("Couldn't factory reset"));
+    }
+  }
+
+
+  /* Disable command echo from Bluefruit */
+  ble.echo(false);
+
+  Serial.println("Requesting Bluefruit info:");
+  /* Print Bluefruit information */
+  ble.info();
+
+  Serial.println(F("Please use Adafruit Bluefruit LE app to connect in UART mode"));
+  Serial.println(F("Then Enter characters to send to Bluefruit"));
+  Serial.println();
+
+  ble.verbose(false);  // debug info is a little annoying after this point!
+
+  /* Wait for connection */
+  while (! ble.isConnected()) {
+    delay(500);
+  }
+
+  // LED Activity command is only supported from 0.6.6
+  if ( ble.isVersionAtLeast(MINIMUM_FIRMWARE_VERSION) )
+  {
+    // Change Mode LED Activity
+    Serial.println(F("******************************"));
+    Serial.println(F("Change LED activity to " MODE_LED_BEHAVIOUR));
+    ble.sendCommandCheckOK("AT+HWModeLED=" MODE_LED_BEHAVIOUR);
+    Serial.println(F("******************************"));
+  }
+}
+
+/**************************************************************************/
+/*!
+    @brief  Constantly poll for new command or response data
+*/
+/**************************************************************************/
+void loop(void)
+{
+
+  //Check for button
+  buttonState = digitalRead(buttonPin);
+  // Check for user input
+  //  char inputs[BUFSIZE + 1];
+  //
+  //  if ( getUserInput(inputs, BUFSIZE) )
+  //  {
+  //    // Send characters to Bluefruit
+  //    Serial.print("[Send] ");
+  //    Serial.println(inputs);
+  //
+  //    ble.print("AT+BLEUARTTX=");
+  //    ble.println(inputs);
+  //  }
+
+  if (buttonState == LOW) {
+    //button sends message to ble
+    
+    if(startRanging == 1){ 
+    ble.println( F("AT+BLEUARTTX=C") );
+    Serial.println("c");
+    startRanging = 0;
+    }
+  }
+
+//  // check response stastus
+//  if (! ble.waitForOK() ) {
+//
+//    Serial.println(F("Failed to send?"));
+//  }
+
+  // Check for incoming characters from Bluefruit
+  ble.println("AT+BLEUARTRX");
+  ble.readline();
+  if (strcmp(ble.buffer, "OK") == 0) {
+    // no data
+    return;
+  }
+  // Some data was found, its in the buffer
+  Serial.print(F("[Recv] ")); Serial.println(ble.buffer);
+
+  startRanging =1;
+  if (ble.buffer > 0) {
+    String inString = String(ble.buffer);
+    Serial.println("string is " + inString);
+    //    if(inString.charAt(0) == 'a'){
+    //      pixels.setPixelColor(0, pixels.Color(0, 150, 50));
+    //      pixels.show();
+    //    }
+    for (int i = 0; i < inString.length(); i++) {
+      if (inString.charAt(i) == 'a') {
+        //wallet on
+        pixels.setPixelColor(1, pixels.Color(0, 150, 50));
+        pixels.show();
+      } else if (inString.charAt(i) == 'b') {
+        //wallet off
+        pixels.setPixelColor(1, pixels.Color(150, 0, 0));
+        pixels.show();
+      }
+      if (inString.charAt(i) == 'c') {
+        //key on
+        pixels.setPixelColor(2, pixels.Color(0, 150, 0));
+        pixels.show();
+      } else if (inString.charAt(i) == 'd') {
+        //key off
+        pixels.setPixelColor(2, pixels.Color(150, 0, 0));
+        pixels.show();
+      }
+      if (inString.charAt(i) == 'e') {
+        //umbrella on
+        pixels.setPixelColor(3, pixels.Color(0, 150, 0));
+        pixels.show();
+      } else if (inString.charAt(i) == 'f') {
+        //umbrella off
+        pixels.setPixelColor(3, pixels.Color(150, 0, 0));
+        pixels.show();
+      }
+      if (inString.charAt(i) == 'g') {
+        //item on
+        pixels.setPixelColor(4, pixels.Color(0, 150, 0));
+        pixels.show();
+      } else if (inString.charAt(i) == 'h') {
+        //item off
+        pixels.setPixelColor(4, pixels.Color(150, 0, 0));
+        pixels.show();
+      }
+    }
+  }
+  ble.waitForOK();
+
+}
+
+/**************************************************************************/
+/*!
+    @brief  Checks for user input (via the Serial Monitor)
+*/
+/**************************************************************************/
+bool getUserInput(char buffer[], uint8_t maxSize)
+{
+  // timeout in 100 milliseconds
+  TimeoutTimer timeout(100);
+
+  memset(buffer, 0, maxSize);
+  while ( (Serial.peek() < 0) && !timeout.expired() ) {}
+
+  if ( timeout.expired() ) return false;
+
+  delay(2);
+  uint8_t count = 0;
+  do
+  {
+    count += Serial.readBytes(buffer + count, maxSize);
+    delay(2);
+  } while ( (count < maxSize) && !(Serial.peek() < 0) );
+
+  return true;
+}
---
+++ pocket-check-cordova/www/js/index.js
@@ -0,0 +1,304 @@
+// (c) 2014 Don Coleman
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/* global mainPage, deviceList, refreshButton */
+/* global detailPage, resultDiv, messageInput, sendButton, disconnectButton */
+/* global ble  */
+/* jshint browser: true , devel: true*/
+'use strict';
+// ASCII only
+
+var deviceIdToSend;
+
+function bytesToString(buffer) {
+    return String.fromCharCode.apply(null, new Uint8Array(buffer));
+}
+
+// ASCII only
+function stringToBytes(string) {
+    var array = new Uint8Array(string.length);
+    for (var i = 0, l = string.length; i < l; i++) {
+        array[i] = string.charCodeAt(i);
+    }
+    return array.buffer;
+}
+
+function sendMsg(message) {
+    console.log("start send");
+    var success = function() {
+    };
+
+    var failure = function(err) {
+        console.log("Failed writing data to the bluefruit le");
+        console.log(err);
+
+    };
+
+    var data = stringToBytes(message);
+
+    if (app.writeWithoutResponse) {
+        ble.writeWithoutResponse(
+            deviceIdToSend,
+            bluefruit.serviceUUID,
+            bluefruit.txCharacteristic,
+            data, success, failure
+        );
+    } else {
+        ble.write(
+            deviceIdToSend,
+            bluefruit.serviceUUID,
+            bluefruit.txCharacteristic,
+            data, success, failure
+        );
+    }
+}
+
+// this is Nordic's UART service
+var bluefruit = {
+    serviceUUID: '6e400001-b5a3-f393-e0a9-e50e24dcca9e',
+    txCharacteristic: '6e400002-b5a3-f393-e0a9-e50e24dcca9e', // transmit is from the phone's perspective
+    rxCharacteristic: '6e400003-b5a3-f393-e0a9-e50e24dcca9e' // receive is from the phone's perspective
+};
+
+
+var identifiers = {
+    green: '792aca5aef8e97b8',
+    purple: 'd1ebb1b1d5b877a5',
+    blue: '71a8aeb13c06db7c',
+    yellow: 'c8e4ff0b9fe72cf7'
+        //    pink: '448856f9d2624680',
+};
+
+var app = {
+    initialize: function() {
+        this.bindEvents();
+    },
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+        refreshButton.addEventListener('touchend', this.refreshDeviceList, false);
+        rangeButton.addEventListener('touchend', this.startRangingNearables, false);
+        disconnectButton.addEventListener('touchend', this.disconnect, false);
+        deviceList.addEventListener('touchend', this.connect, false); // assume not scrolling
+    },
+    onDeviceReady: function() {
+        app.refreshDeviceList();
+    },
+    refreshDeviceList: function() {
+        deviceList.innerHTML = ''; // empties the list
+        if (cordova.platformId === 'android') { // Android filtering is broken
+            ble.scan([bluefruit.serviceUUID], 5, app.onDiscoverDevice, app.onError);
+        } else {
+            ble.scan([bluefruit.serviceUUID], 5, app.onDiscoverDevice, app.onError);
+        }
+    },
+    onDiscoverDevice: function(device) {
+        var listItem = document.createElement('li'),
+            html = '<b>' + device.name + '</b><br/>' +
+            'RSSI: ' + device.rssi + '&nbsp;|&nbsp;' +
+            device.id;
+
+        listItem.dataset.deviceId = device.id;
+        listItem.innerHTML = html;
+        deviceList.appendChild(listItem);
+    },
+
+    connect: function(e) {
+        var deviceId = e.target.dataset.deviceId,
+            onConnect = function(peripheral) {
+                app.determineWriteType(peripheral);
+
+                // subscribe for incoming data
+                ble.startNotification(deviceId, bluefruit.serviceUUID, bluefruit.rxCharacteristic, app.onData, app.onError);
+                //sendButton.dataset.deviceId = deviceId;
+                disconnectButton.dataset.deviceId = deviceId;
+                // app.showDetailPage();
+            };
+        deviceIdToSend = deviceId;
+        ble.connect(deviceId, onConnect, app.onError);
+    },
+    determineWriteType: function(peripheral) {
+        // Adafruit nRF8001 breakout uses WriteWithoutResponse for the TX characteristic
+        // Newer Bluefruit devices use Write Request for the TX characteristic
+
+        var characteristic = peripheral.characteristics.filter(function(element) {
+            if (element.characteristic.toLowerCase() === bluefruit.txCharacteristic) {
+                return element;
+            }
+        })[0];
+
+        if (characteristic.properties.indexOf('WriteWithoutResponse') > -1) {
+            app.writeWithoutResponse = true;
+        } else {
+            app.writeWithoutResponse = false;
+        }
+
+    },
+    onData: function(data) { // data received from Arduino
+        //console.log(data);
+        // resultDiv.innerHTML = resultDiv.innerHTML + "Received: " + bytesToString(data) + "<br/>";
+        // resultDiv.scrollTop = resultDiv.scrollHeight;
+         var inString = bytesToString(data);
+         //console.log("string " + inString);
+         var inChar = inString.charAt(0);
+        if(inChar == 'C'){
+            console.log("start range scan");
+            app.startRangingNearables();
+        }
+    },
+    disconnect: function(event) {
+        var deviceId = event.target.dataset.deviceId;
+        ble.disconnect(deviceId, app.jacketOff, app.onError);
+    },
+    onError: function(reason) {
+        console.log("ERROR: " + reason); // real apps should use notification.alert
+    },
+
+    startRangingNearables: function() {
+
+        var onRange = function(nearable) {
+            var status;
+            if (nearable.rssi > -90) {
+                console.log(nearable.nameForType + " on"+","+nearable.rssi);
+                status = true;
+                setNeopixels(nearable.nameForType, status);
+            } else {
+                console.log(nearable.nameForType + " off"+","+nearable.rssi);
+                status = false;
+                setNeopixels(nearable.nameForType, status);
+            }
+            //displayNearablesInfo(nearable);
+            estimote.nearables.stopRangingForIdentifier(
+                nearable.identifier,
+                function() {
+                    console.log("stopped");
+                },
+                onError
+            );
+
+        };
+
+        var setNeopixels = function(name, status) {
+            if (name == "bag") {
+                // neopixels.wallet = status;
+                if (status == true) {
+                    sendMsg("a");
+
+                } else {
+                    sendMsg("b");
+
+                }
+            } else if (name == "bike") {
+                // neopixels.key = status;
+                if (status == true) {
+                    sendMsg("c");
+
+                } else {
+                    sendMsg("d");
+
+                }
+
+            } else if (name == "dog") {
+                // neopixels.umbrella = status;
+                if (status == true) {
+                    sendMsg("e");
+
+                } else {
+                    sendMsg("f");
+
+                }
+
+            } else if (name == "generic") {
+                // neopixels.item == status;
+                if (status == true) {
+                    sendMsg("g");
+
+                } else {
+                    sendMsg("h");
+
+                }
+
+            }
+        }
+
+
+        var onError = function(errorMessage) {
+            console.log('Range error: ' + errorMessage);
+        };
+
+        var displayNearablesInfo = function(nearables) {
+            console.log("start display");
+            console.log(nearables);
+            // Clear HTML list.
+            $('#id-screen-range-nearables .style-item-list').empty();
+
+            // Generate HTML for nearables.
+            $.each(nearables, function(i, nearable) {
+                var element = $(createNearableHTML(nearable));
+                $('#id-screen-range-nearables .style-item-list').append(element);
+            });
+        };
+
+        var createNearableHTML = function(nearable) {
+            // console.log("create html");
+            // var colorClasses = 'style-color-blueberry-dark style-color-blueberry-dark-text';
+            // var htm = '<div class="' + colorClasses + '">' + '<table><tr><td>Type</td><td>' + nearable.nameForType + '</td></tr><tr><td>Identifier</td><td>' + nearable.identifier + '</td><tr><td>Color</td><td>' + nearable.nameForColor + '</td></tr><tr><td>Zone</td><td>' + nearable.zone + '</td></tr><tr><td>RSSI</td><td>' + nearable.rssi + '</td></tr><tr><td>Temperature</td><td>' + nearable.temperature + '</td></tr><tr><td>Is moving</td><td>' + (nearable.isMoving ? 'Yes' : 'No') + '</td></tr><tr><td>xAcceleration</td><td>' + nearable.xAcceleration + '</td></tr><tr><td>yAcceleration</td><td>' + nearable.yAcceleration + '</td></tr><tr><td>zAcceleration</td><td>' + nearable.zAcceleration + '</td></tr></table></div>';
+            // return htm;
+        };
+
+        $.each(identifiers, function(i, id) {
+            estimote.nearables.startRangingForIdentifier(
+                id,
+                onRange,
+                onError
+            );
+
+
+        });
+
+    },
+
+    //UI changes
+
+    jacketOn: function() {
+
+    },
+
+    jacketOff: function() {
+        //UI Jacket Off
+    },
+
+    walletOn: function() {
+
+    },
+
+    walletOff: function() {
+        //UI Jacket Off
+    },
+
+    keyOn: function() {
+
+    },
+
+    keyOff: function() {
+        //UI Jacket Off
+    },
+    objectOn: function() {
+
+    },
+
+    objectOff: function() {
+        //UI Jacket Off
+    }
+
+};
